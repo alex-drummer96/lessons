d9e5806786ac6346de54b30e7c928493beca7583
Alex <sashapavlov559@gmail.com>
2022-06-26 21:28:43 +0300

    произвел действия над множествами садовых и луговых цветов

--- lesson_002/08_garden.py
+++ lesson_002/08_garden.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # в саду сорвали цветы
@@ -8,20 +7,22 @@ garden = ('ромашка', 'роза', 'одуванчик', 'ромашка', 
 meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер', 'мак', 'одуванчик', 'ромашка', )
 
 # создайте множество цветов, произрастающих в саду и на лугу
-# garden_set =
-# meadow_set =
-# TODO здесь ваш код
+garden_set = set(garden)
+meadow_set = set(meadow)
+
 
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
-
+all_flowers = garden_set | meadow_set
+print(all_flowers)
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
-
+same_flowers = garden_set & meadow_set
+print(same_flowers)
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
+only_garden = garden_set - meadow_set
+print(only_garden)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+only_meadow = meadow_set - garden_set
+print(only_meadow)
 
 
