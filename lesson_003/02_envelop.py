0ed983b1d4a80b1e7316639d7a8a41f72575e2f4
Alex <sashapavlov559@gmail.com>
2022-06-29 07:49:28 +0300

    сделал условие для проверки размеров объектов на вхождение

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -20,7 +20,13 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x >= paper_x:
+    if envelop_y >= paper_y:
+        print('ДА')
+    else:
+        print("НЕТ")
+else:
+    print("НЕТ")
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,8 +34,8 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -49,4 +55,17 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if 0 > brick_x > 1000:
+    print('ValueError')
+elif 0 > brick_y > 1000:
+    print('ValueError')
+elif 0 > brick_z > 1000:
+    print('ValueError')
+else:
+    if hole_x >= brick_x:
+        if hole_y >= brick_y:
+            print('yes')
+        else:
+            print('no')
+    else:
+        print('no')
