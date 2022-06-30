5836357b1b427664cae12fc0a29cbc0bb4d06f83
Alex <sashapavlov559@gmail.com>
2022-06-30 15:11:29 +0300

    с помощью цикла for раситал общее количество мебели на склоадах и ее стоимость

--- lesson_003/05_store.py
+++ lesson_003/05_store.py
@@ -46,7 +46,21 @@ store = {
 #         подсчет стоимости товара
 #     вывод на консоль количества и стоимости товара на складе
 
-# TODO здесь ваш код
+for name, code in goods.items():
+    local_quantity = 0
+    local_price = 0
+    total_quantity = 0
+    total_cost = 0
+    for some_dict in store[code]:
+        for key in some_dict:
+            if key == 'quantity':
+                local_quantity = some_dict[key]
+            else:
+                local_price = some_dict[key]
+        total_quantity += local_quantity
+        total_cost += local_quantity * local_price
+    print(name, '-', total_quantity, 'шт, стоимость', total_cost, 'руб')
+
 
 
 
