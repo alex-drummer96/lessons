1d99a867ca8ce9187a09d637c7f759b402b45e80
Alex <sashapavlov559@gmail.com>
2022-07-07 23:07:14 +0300

    написал ф-цию для рисования снегопада

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+import random
+
+sd.resolution = (1200, 600)
 
 
 # На основе кода из практической части реализовать снегопад:
@@ -8,7 +11,6 @@ import simple_draw as sd
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
 
 # Пригодятся функции
 # sd.get_point()
@@ -16,16 +18,42 @@ N = 20
 # sd.sleep()
 # sd.random_number()
 # sd.user_want_exit()
+def draw_snowflake(x, y, length):
+    while y >= 0:
+        for i in range(2):
+            sd.start_drawing()
+            point = sd.get_point(x, y)
+            if i == 0:
+                snow = sd.snowflake(center=point, length=length)
+            else:
+                if y > 0:
+                    snow = sd.snowflake(center=point, length=length, color=sd.background_color)
+                y -= 20
+                x -= sd.random_number(-100, 100)
+            sd.finish_drawing()
+            sd.sleep(.03)
+            if sd.user_want_exit():
+                break
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+
+x_list = []
+y_list = []
+length_list = []
+for n in range(20):
+    x = sd.random_number(50, 550)
+    y = random.randrange(400, 600, 100)
+    length = sd.random_number(10, 100)
+    x_list.append(x)
+    y_list.append(y)
+    length_list.append(length)
+
+N = 20
+while N > 0:
+    x_0 = x_list[N - 1]
+    y_0 = y_list[N - 1]
+    length_0 = length_list[N - 1]
+    draw_snowflake(x=x_0, y=y_0, length=length_0)
+    N -= 1
 
 sd.pause()
 
