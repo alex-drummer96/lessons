2cc8be6810dcc82d2283e6a6d0a3f1219e179e91
Alex <sashapavlov559@gmail.com>
2022-07-08 19:12:06 +0300

    сделал импорты списков из модулей.

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,6 +3,8 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+import room_1, room_2
 
 
+print("в комнате room_1 живут:", room_1.folks)
+print("в комнате room_2 живут:", room_2.folks)
