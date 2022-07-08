7d0b07de5fac658fad227413b7b0ac9e3e104f40
Alex <sashapavlov559@gmail.com>
2022-07-08 20:10:18 +0300

    вывел строку со всем жителями района

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -2,11 +2,25 @@
 
 # Составить список всех живущих на районе и Вывести на консоль через запятую
 # Формат вывода: На районе живут ...
-# подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
+#  подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
+from district.central_street.house1.room1 import folks as folks_1
+from district.central_street.house1.room2 import folks as folks_2
+from district.central_street.house2.room1 import folks as folks_3
+from district.central_street.house2.room2 import folks as folks_4
+from district.soviet_street.house1.room1 import folks as folks_5
+from district.soviet_street.house1.room2 import folks as folks_6
+from district.soviet_street.house2.room1 import folks as folks_7
+from district.soviet_street.house2.room2 import folks as folks_8
 
-# TODO здесь ваш код
+folks_list = [folks_1, folks_2, folks_3, folks_4, folks_5, folks_6, folks_7, folks_8]
+new_list = []
+string = ', '
+for i in range(len(folks_list)):
+    for n in range(len(folks_list[i])):
+        new_list.append(folks_list[i][n])
 
+print('На районе живут', string.join(new_list))
 
 
 
