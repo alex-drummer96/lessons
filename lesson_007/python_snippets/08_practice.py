b93ab80c345356818106fb61799ec0d51d0fc642
Alex <sashapavlov559@gmail.com>
2022-07-22 23:06:59 +0300

    emulation of human life

--- lesson_007/python_snippets/08_practice.py
+++ lesson_007/python_snippets/08_practice.py
@@ -1,61 +1,50 @@
-# -*- coding: utf-8 -*-
-
 from random import randint
-
-
-# Реализуем модель человека.
-# Человек может есть, работать, играть, ходить в магазин.
-# У человека есть степень сытости, немного еды и денег.
-# Если сытость < 0 единиц, человек умирает.
-# Человеку надо прожить 365 дней.
 from termcolor import cprint
 
 
 class Man:
-
     def __init__(self, name):
-        self.name = name
         self.fullness = 50
+        self.name = name
         self.house = None
 
     def __str__(self):
-        return 'Я - {}, сытость {}'.format(
-            self.name, self.fullness)
+        return f'I am - {self.name}, fullness - {self.fullness},'
 
     def eat(self):
         if self.house.food >= 10:
-            cprint('{} поел'.format(self.name), color='yellow')
+            print(f'{self.name} ate')
             self.fullness += 10
             self.house.food -= 10
         else:
-            cprint('{} нет еды'.format(self.name), color='red')
+            print(f'{self.name} has no food')
 
     def work(self):
-        cprint('{} сходил на работу'.format(self.name), color='blue')
+        print(f'{self.name} went to work')
         self.house.money += 50
         self.fullness -= 10
 
-    def watch_MTV(self):
-        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
+    def play_game(self):
+        print(f'{self.name} played games')
         self.fullness -= 10
 
     def shopping(self):
         if self.house.money >= 50:
-            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            print(f'{self.name} went shopping')
             self.house.money -= 50
             self.house.food += 50
         else:
-            cprint('{} деньги кончились!'.format(self.name), color='red')
+            print('No money - No honey')
 
-    def go_to_the_house(self, house):
+    def settle_in_house(self, house):
         self.house = house
         self.fullness -= 10
-        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
+        print(f'{self.name} settle in a house')
 
     def act(self):
         if self.fullness <= 0:
-            cprint('{} умер...'.format(self.name), color='red')
-            return
+            print(f'{self.name} died')
+            return False
         dice = randint(1, 6)
         if self.fullness < 20:
             self.eat()
@@ -68,40 +57,31 @@ class Man:
         elif dice == 2:
             self.eat()
         else:
-            self.watch_MTV()
+            self.play_game()
 
 
 class House:
 
     def __init__(self):
-        self.food = 50
-        self.money = 0
+        self.food = 10
+        self.money = 50
 
     def __str__(self):
-        return 'В доме еды осталось {}, денег осталось {}'.format(
-            self.food, self.money)
+        return f"В доме осталось еды:{self.food}, в доме соталось денег: {self.money}"
 
-
-citizens = [
-    Man(name='Бивис'),
-    Man(name='Батхед'),
-    Man(name='Кенни'),
-]
-
-
-my_sweet_home = House()
-for citisen in citizens:
-    citisen.go_to_the_house(house=my_sweet_home)
+beavis = Man(name='Beavis')
+butthead = Man(name='Butthead')
+house1 = House()
+beavis.settle_in_house(house=house1)
+butthead.settle_in_house(house=house1)
 
 for day in range(1, 366):
-    print('================ день {} =================='.format(day))
-    for citisen in citizens:
-        citisen.act()
-    print('--- в конце дня ---')
-    for citisen in citizens:
-        print(citisen)
-    print(my_sweet_home)
+    cprint(f'******************************** DAY: {day} *******************************', color='yellow')
+    beavis.act()
+    butthead.act()
+    cprint(f'*********************** at the end of the day: {day} ***********************', color='yellow')
+    cprint(house1, color='magenta')
+    cprint(beavis, color='blue')
+    cprint(butthead, color='green')
 
-# Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
-# Нужен класс Дом, в нем должн быть холодильник с едой и тумбочка с деньгами
-# Еда пусть хранится в холодильнике в доме, а деньги - в тумбочке.
+
