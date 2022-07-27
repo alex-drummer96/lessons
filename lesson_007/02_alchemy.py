2164a57a0d85ded36b6d483c8ccb20f1242cc020
Alex <sashapavlov559@gmail.com>
2022-07-27 22:21:36 +0300

    created a prototype of the game Alchemy

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,160 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water:
+
+    def __init__(self):
+        self.name = 'water'
+
+    def __add__(self, other):
+        if other.name == 'air':
+            new_obj = Storm()
+            return new_obj
+        elif other.name == 'fire':
+            new_obj = Steam()
+            return new_obj
+        elif other.name == 'ground':
+            new_obj = Dirt()
+            return new_obj
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Air:
+
+    def __init__(self):
+        self.name = 'air'
+
+    def __add__(self, other):
+        if other.name == 'water':
+            new_obj = Storm()
+            return new_obj
+        elif other.name == 'fire':
+            new_obj = Lightning()
+            return new_obj
+        elif other.name == 'ground':
+            new_obj = Dust()
+            return new_obj
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Fire:
+
+    def __init__(self):
+        self.name = 'fire'
+
+    def __add__(self, other):
+        if other.name == 'air':
+            new_obj = Lightning()
+            return new_obj
+        elif other.name == 'water':
+            new_obj = Steam()
+            return new_obj
+        elif other.name == 'ground':
+            new_obj = Lava()
+            return new_obj
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Ground:
+
+    def __init__(self):
+        self.name = 'ground'
+
+    def __add__(self, other):
+        if other.name == 'air':
+            new_obj = Dust()
+            return new_obj
+        elif other.name == 'fire':
+            new_obj = Lava()
+            return new_obj
+        elif other.name == 'water':
+            new_obj = Dirt()
+            return new_obj
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Storm:
+
+    def __init__(self):
+        self.name = 'storm'
+
+    def __str__(self):
+        return self.name
+
+
+class Steam:
+
+    def __init__(self):
+        self.name = 'steam'
+
+    def __str__(self):
+        return self.name
+
+
+class Dirt:
+
+    def __init__(self):
+        self.name = 'dirt'
+
+    def __str__(self):
+        return self.name
+
+
+class Lightning:
+
+    def __init__(self):
+        self.name = 'lightning'
+
+    def __str__(self):
+        return self.name
+
+
+class Dust:
+
+    def __init__(self):
+        self.name = 'dust'
+
+    def __str__(self):
+        return self.name
+
+
+class Lava:
+
+    def __init__(self):
+        self.name = 'lava'
+
+    def __str__(self):
+        return self.name
+
+
+print(Water() + Air())
+print(Water() + Fire())
+print(Water() + Ground())
+print(Air() + Water())
+print(Air() + Fire())
+print(Air() + Ground())
+print(Fire() + Water())
+print(Fire() + Air())
+print(Fire() + Ground())
+print(Ground() + Fire())
+print(Ground() + Air())
+print(Ground() + Water())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
